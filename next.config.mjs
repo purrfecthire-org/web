5010cad91d9dc5afcf57eaaad7a83a363ac5171b
elpapi42 <whitman-2@hotmail.com>
2024-05-12 14:13:47 -0500

    Add basepath again

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    basePath: isProd ? '/web' : '',
+    basePath: '/web',
     output: "export",
     images: {
         remotePatterns: [
