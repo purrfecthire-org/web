89e42f7d3662e51b9476a7496de09dde6812b6c4
joset-asperjet <104882842+joset-asperjet@users.noreply.github.com>
2024-06-07 17:09:04 -0500

    Please enter the commit message for your changes. Lines starting

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -26,7 +26,7 @@ export default function MainContent() {
               onClick={(event) =>
                 redirect(
                   event,
-                  "https://calendly.com/daniel-aguirred/purrfect-hire-eng?month=2024-03"
+                  "https://calendly.com/kate-forero/"
                 )
               }>
               Talk to sales
